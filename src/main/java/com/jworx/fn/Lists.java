3a310dbb1a7c9849524ac5715392071edf9e47d7
dalbrekt <tony@dalbrekt.com>
2011-09-29 20:04:32 +0200

    Added factory for array and list

--- src/main/java/com/jworx/fn/Lists.java
+++ src/main/java/com/jworx/fn/Lists.java
@@ -1,5 +1,7 @@
 package com.jworx.fn;
 
+import java.util.Arrays;
+
 /**
  * Functional list module.
  * 
@@ -9,21 +11,21 @@ public class Lists {
 		T head();
 
 		List<T> tail();
-		
+
 		List<T> filter(Function1<T, Boolean> f);
 
 		boolean isEmpty();
-		
-		public <T2> List<T2> map (Function1<T, T2> f);
-		
+
+		public <T2> List<T2> map(Function1<T, T2> f);
+
 		public <T2> T2 foldLeft(T2 seed, Function2<T2, T, T2> f);
-		
+
 		public <T2> T2 foldRight(T2 seed, Function2<T, T2, T2> f);
-		
+
 		public void foreach(Function1Void<T> f);
 		
-		public void foreach(Function2Void<T, Integer> f, Integer index);
-		
+		public Integer size();
+
 	}
 
 	/**
@@ -53,35 +55,39 @@ public class Lists {
 		public boolean isEmpty() {
 			return false;
 		}
-		
+
 		public List<T> filter(Function1<T, Boolean> f) {
-			if(f.apply(head())) {
+			if (f.apply(head())) {
 				return list(head(), tail().filter(f));
 			} else {
 				return tail().filter(f);
 			}
 		}
-		
+
 		public <T2> List<T2> map(Function1<T, T2> f) {
 			return list(f.apply(head()), tail.map(f));
 		}
-		
+
 		public <T2> T2 foldLeft(T2 seed, Function2<T2, T, T2> f) {
 			return tail().foldLeft(f.apply(seed, head()), f);
 		}
-		
-		public <T2> T2 foldRight(T2 seed, com.jworx.fn.Function2<T,T2,T2> f) {
+
+		public <T2> T2 foldRight(T2 seed, com.jworx.fn.Function2<T, T2, T2> f) {
 			return f.apply(head(), tail().foldRight(seed, f));
 		};
-		
+
 		public void foreach(Function1Void<T> f) {
 			f.apply(head());
 			tail().foreach(f);
 		}
 		
-		public void foreach(Function2Void<T, Integer> f, Integer index) {
-			f.apply(head(), index);
-			tail().foreach(f, index + 1);
+		public Integer size() {
+			return foldLeft(0, new Function2<Integer, T, Integer>() {
+
+				public Integer apply(Integer arg1, T arg2) {
+					return arg1 + 1;
+				}
+			});
 		}
 
 		@Override
@@ -130,26 +136,28 @@ public class Lists {
 		public String toString() {
 			return "()";
 		};
-		
-		public Lists.List<Object> filter(com.jworx.fn.Function1<Object,Boolean> f) {
+
+		public Lists.List<Object> filter(com.jworx.fn.Function1<Object, Boolean> f) {
 			return this;
 		};
-		
-		public <T2> Lists.List<T2> map(com.jworx.fn.Function1<Object,T2> f) {
+
+		public <T2> Lists.List<T2> map(com.jworx.fn.Function1<Object, T2> f) {
 			return emptyList();
 		};
-		
-		public <T2> T2 foldLeft(T2 seed, com.jworx.fn.Function2<T2,Object,T2> f) {
+
+		public <T2> T2 foldLeft(T2 seed, com.jworx.fn.Function2<T2, Object, T2> f) {
 			return seed;
 		};
-		
+
 		public <T2> T2 foldRight(T2 seed, com.jworx.fn.Function2<Object, T2, T2> f) {
 			return seed;
 		};
+
+		public void foreach(com.jworx.fn.Function1Void<Object> f) {
+		};
 		
-		public void foreach(com.jworx.fn.Function1Void<Object> f) {};
-		
-		public void foreach(com.jworx.fn.Function2Void<Object,Integer> f, Integer index) {};
+		public Integer size() {return 0;};
+
 	};
 
 	/**
@@ -163,7 +171,7 @@ public class Lists {
 	}
 
 	/**
-	 * Factory to create a non empty list
+	 * Factory that creates a non empty list
 	 * 
 	 * @param head head of list
 	 * @param tail tail of list
@@ -171,5 +179,32 @@ public class Lists {
 	 */
 	public static <T> List<T> list(T head, List<T> tail) {
 		return new NonEmptyList<T>(head, tail);
+	}
+
+	/**
+	 * Factory that creates a list from an array.
+	 * 
+	 * @param array source array
+	 * @return
+	 */
+	public static <T> List<T> list(T... array) {
+		List<T> list = emptyList();
+		if (array.length > 0) {
+			list = list(array[0], list(Arrays.copyOfRange(array, 1, array.length)));
+		}
+		return list;
+	}
+
+	/**
+	 * Factory that creates a list from a java.util.List,
+	 * @param sourceList
+	 * @return
+	 */
+	public static <T> List<T> list(java.util.List<T> sourceList) {
+		List<T> list = emptyList();
+		if (sourceList.size() > 0) {
+			list = list(sourceList.get(0), list(sourceList.subList(1, sourceList.size())));
+		}
+		return list;
 	}
 }
